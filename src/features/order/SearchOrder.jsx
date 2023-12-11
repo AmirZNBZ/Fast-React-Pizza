083400fad1a6134eb6b95b6ab9fc4b5ad288be3f
amir nabiizade <amirnabiizade@gmail.com>
2023-12-11 14:31:29 +0330

    added tailwind to the app and use the tailwind and set a laoder

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,6 +16,12 @@ function SearchOrder() {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="place w-28 rounded-full bg-yellow-100
+        px-4 py-2 text-sm placeholder:text-stone-400
+        sm:focus:w-72 sm:w-64 transition-all duration-300
+        focus:ring focus:outline-none focus:ring-yellow-500
+        focus:ring-opacity-50
+        "
       />
     </form>
   );
