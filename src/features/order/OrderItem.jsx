b6c31f59f6da935a72d5426209cb3bec66408a4b
amir nabiizade <amirnabiizade@gmail.com>
2023-12-22 17:36:16 +0330

    tailwind styles finished

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -4,11 +4,14 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
 
   return (
     <li className="py-3">
-      <div>
+      <div
+        className="flex items-center justify-between 
+      gap-4 text-sm"
+      >
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
